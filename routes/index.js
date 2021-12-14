6486238012426cae58bd3351d18f9f59416e855b
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-12-14 15:17:10 -0300

    EJS includes

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,27 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/contacts', function(req, res, next){
+    res.render('contacts', {
+      title: 'Contato - Restaurante saboroso!'
+    })
+})
+router.get('/menus', function(req, res, next){
+  res.render('contacts', {
+    title: 'Menus - Restaurante saboroso!'
+  })
+})
+router.get('/reservations', function(req, res, next){
+  res.render('contacts', {
+    title: 'Reservas - Restaurante saboroso!'
+  })
+})
+router.get('/service', function(req, res, next){
+  res.render('contacts', {
+    title: 'Serviços - Restaurante saboroso!'
+  })
+})
+
+
+
 module.exports = router;
